b92d08b977324ec35389e30dac3cbd7dbf875542
suhaib079 <suhaibabdennabi33@gmail.com>
2021-03-17 20:03:59 +0200

    ghj

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express');
 const server = express();
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.get('/hi',(req,res)=>{
    let family = [
        {name:'mo'},
